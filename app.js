1ae60f4e0b11b00e5b9a197552ffa1b7ad441127
Coatzel <Coatzl@Christophers-MacBook-Pro.local>
2014-09-03 18:56:13 -0400

    hot and cold game

---
+++ app.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+	
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+/* new game */
+$(".new").click(function(){
+	window.location.reload(true);
+	
+});
+
+/*random answer */
+
+var randomAnswer = Math.floor((Math.random() * 99) + 1);
+console.log(randomAnswer);
+
+
+
+/* guess submit and clear */
+$("form").on('click', '#guessButton', function(event){
+	event.preventDefault();
+	var submitAnswer =$("#userGuess").val();
+	console.log(submitAnswer)
+	$('#guessList').append('<li>'+$('#userGuess').val()+'</li>');
+	$("#userGuess").val(" ");
+	if (randomAnswer == submitAnswer){
+		alert("Epic Win")
+	};
+
+    
+  });
+
+	
+
+});
+
+
+
+
+
+
+
+
